86324e18e950a72f2583b76dfb86c4529457c7f0
Ксения <korzhuk.xeniya@yandex.ru>
2023-07-31 12:33:33 +0300

    done

--- src/main/java/com/skypro/exception1/controller/RegistrationController.java
+++ src/main/java/com/skypro/exception1/controller/RegistrationController.java
@@ -2,27 +2,27 @@ package com.skypro.exception1.controller;
 
 import com.skypro.exception1.WrongLoginException;
 import com.skypro.exception1.WrongPasswordException;
-import com.skypro.exception1.service.Service;
-import com.skypro.exception1.service.ServiceImpl;
+import com.skypro.exception1.service.RegistrationService;
+import com.skypro.exception1.service.RegistrationServiceImpl;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class Controller {
-    private Service service;
+public class RegistrationController {
+    private RegistrationService service;
 
-    public Controller(Service service) {
+    public RegistrationController(RegistrationService service) {
         this.service = service;
     }
 
     @GetMapping
 
-    public static String registrationOnWebsite(@RequestParam("login") String login,
+    public  String registrationOnWebsite(@RequestParam("login") String login,
                                                @RequestParam("password") String password,
                                                @RequestParam("confirmPassword") String confirmPassword) {
         try {
-            ServiceImpl.registrationOnWebsite(login, password, confirmPassword);
+            service.registrationOnWebsite(login, password, confirmPassword);
 
         } catch (WrongLoginException | WrongPasswordException ex) {
             return(ex.getMessage());
--- src/main/java/com/skypro/exception1/service/RegistrationServiceImpl.java
+++ src/main/java/com/skypro/exception1/service/RegistrationServiceImpl.java
@@ -2,37 +2,41 @@ package com.skypro.exception1.service;
 
 import com.skypro.exception1.WrongLoginException;
 import com.skypro.exception1.WrongPasswordException;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.regex.Pattern;
 
-public interface Service {
-    static Pattern regex = Pattern.compile("^[A-Za-z0-9_]{0,20}$");
+@org.springframework.stereotype.Service
+public class RegistrationServiceImpl implements RegistrationService {
 
-    static void registrationOnWebsite(String login, String password, String confirmPassword) throws WrongPasswordException {
+    private Pattern regex = Pattern.compile("^[A-Za-z0-9_]{0,20}$");
+
+    @Override
+    public void registrationOnWebsite(String login, String password, String confirmPassword) throws WrongPasswordException {
         checkLogin(login);
         checkPassword(password);
         checkEqualsPassword(password, confirmPassword);
     }
 
-    static void checkLogin(String login) {
+
+    private void checkLogin(String login) throws WrongLoginException {
         if (!login.matches(regex.pattern())) {
             throw new WrongLoginException(" Login должен содержать в себе только латинские буквы, цифры и знак подчеркивания " +
                     "и быть не длиннее 20 символов");
         }
     }
 
-    static void checkPassword(String password) throws WrongPasswordException {
+
+    private void checkPassword(String password) throws WrongPasswordException {
         if (!password.matches(regex.pattern())) {
             throw new WrongPasswordException(" Пароль должен содержать в себе только латинские буквы, цифры и знак подчеркивания " +
                     "и быть не длиннее 20 символов");
         }
     }
 
-    static void checkEqualsPassword(String password, String confirmPassword) {
+
+    private void checkEqualsPassword(String password, String confirmPassword) throws WrongPasswordException {
         if (!password.equals(confirmPassword)) {
-            throw new WrongLoginException(" Пароли не совпадают!");
+            throw new WrongPasswordException(" Пароли не совпадают!");
         }
     }
 }
-
